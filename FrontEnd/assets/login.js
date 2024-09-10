2d67d53897aeb9084c6307c0a41989eddeeeeecd
Dav Prat <scuderia1980@gmail.com>
2024-09-10 16:05:01 +0200

    page login terminé recherche d'erreurs

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -2,10 +2,42 @@
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
 
-const form = document.getElementById("form");
+// récupération du formulaire
+const form = document.querySelector("form");
 
 console.log(passwordInput)
 
 form.addEventListener('submit', function(event) {
     event.preventDefault(); // Empêche le rechargement de la page
+    // collecte les données du formulaire
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+        //envoi des données à l'api requête POST au serveur
+        fetch("http://localhost:5678/api/users/login", {
+    
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+        email: email,
+        password: password
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.token) {
+            // Stocker le token dans le localStorage pour la session de l'utilisateur
+            localStorage.setItem("token", data.token);
+            // Rediriger l'utilisateur vers la page d'accueil
+            window.location.href = "index.html";
+        } else {
+            // Afficher un message d'erreur
+            alert("Email ou mot de passe incorrect.");
+        }
+    })
+    .catch(error => {
+        console.error("Erreur de connexion", error);
+    });
 });
