69b6e3adb5b3b2f11f205595c590451c600b5fb3
Dav Prat <scuderia1980@gmail.com>
2024-09-12 16:24:05 +0200

    switch du login et logout mode admin

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,3 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Vérifier si l'utilisateur est connecté
+    const token = localStorage.getItem("token");
+    const loginButton = document.querySelector('.bouton-login'); // Remplacer par la classe exacte du bouton login
+    const boutonsFiltres = document.getElementById("boutonsFiltres"); //récupère l'id dans le html
+        if (token && boutonsFiltres) {
+        //masque les boutons filtres
+        boutonsFiltres.style.display = 'none';   
+        // Si connecté, remplacer "login" par "logout"
+        loginButton.innerText = 'Logout';
+        loginButton.addEventListener('click', function() {
+            localStorage.removeItem('token'); //supprime le token pour déconnecter l'utilisateur
+            window.location.href = "../index.html"; // Recharge la page pour revenir en mode non-admin            
+            });
+        }
+    });
 //récupère les les champs email et password
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
@@ -12,7 +28,6 @@ form.addEventListener('submit', function(event) {
     // collecte les données du formulaire
     const email = emailInput.value;
     const password = passwordInput.value;
-
         //envoi des données à l'api requête POST au serveur
         fetch("http://localhost:5678/api/users/login", {
     
@@ -21,17 +36,20 @@ form.addEventListener('submit', function(event) {
             'content-type': 'application/json'
         },
         body: JSON.stringify({
-        email: email,
-        password: password
+            email: email,
+            password: password
         })
     })
-    .then(response => response.json())
-    .then(data => {
+    //fonction pour gérer les promesses
+    .then(response => response.json()) // convertit la réponse au format json
+    .then(data => {//traite les données json de l'api
         if (data.token) {
             // Stocker le token dans le localStorage pour la session de l'utilisateur
             localStorage.setItem("token", data.token);
             // Rediriger l'utilisateur vers la page d'accueil
             window.location.href = "../index.html";
+            console.log("Redirection vers : ../index.html");
+            
         } else {
             // Afficher un message d'erreur
             alert("Email ou mot de passe incorrect");
@@ -40,4 +58,5 @@ form.addEventListener('submit', function(event) {
     .catch(error => {
         console.error("Erreur de connexion", error);
     });
+    
 });
--- FrontEnd/assets/modale.js
+++ FrontEnd/assets/modale.js
@@ -9,7 +9,29 @@ const openModale = function (e) {
     modale.addEventListener('click', closeModale)
     modale.querySelector('.js-modale-close').addEventListener('click', closeModale)
     modale.querySelector('.js-modale-stop').addEventListener('click', stopPropagation)
+    // Ajoute les éléments de la galerie dans la modale
+    addGalleryModale()
+}
+// récupère la réf du container de la modale
+const modaleGalleryContainer = document.querySelector('.gallery-modale');
+// fonction pour afficher les éléments dans la modale
+const addGalleryModale = function () {
+    // Vide le conteneur avant d'ajouter des éléments (si déjà chargé)
+    modaleGalleryContainer.innerHTML = '';
+    // réutilise le code de galerie
+    works.forEach(work => {
+       const figure = document.createElement("figure");
+       let img = document.createElement("img");
+       img.src = work.imageUrl;
+       img.alt = work.title;
+       figure.appendChild(img);
 
+       /*const figcaption = document.createElement("figcaption");
+       figcaption.innerText = work.title;
+       figure.appendChild(figcaption);*/
+
+       modaleGalleryContainer.appendChild(figure);
+    })
 }
 
 const closeModale = function (e) {
